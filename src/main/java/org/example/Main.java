84f69d27e76dba36fc1d4bf6b8e021609c369d69
Olga8I <olga.filonchik@gmail.com>
2024-06-17 20:59:17 +0300

    Third

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        String text = "РЫССИЯ РАДИНА";
+        String correctedText = Arrays.stream(text.split("\\s+"))
+                .map(word -> word.replaceAll("А(?!$)", "О"))
+                .collect(Collectors.joining(" "));
+        System.out.println(correctedText);
+    }
+}
