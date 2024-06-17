68c8ba0e8a1a60d703179c2c979419c39c03e0f2
Olga8I <olga.filonchik@gmail.com>
2024-06-17 21:15:19 +0300

    Fifth

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String text = "This is a sample text with the word 'example' written in it.";
+        String substring = "ple";
+        String insertWord = "insertion";
+
+        String result = Arrays.stream(text.split("\\b"))
+                .map(word -> word.endsWith(substring) ? word + " " + insertWord : word)
+                .collect(Collectors.joining());
+
+        System.out.println(result);
     }
 }
