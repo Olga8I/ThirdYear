eb4d3e326421441ef6702c140efa59ef0939a373
Olga8I <olga.filonchik@gmail.com>
2024-06-17 21:23:59 +0300

    Ninth

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
-
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Duis sagittis turpis sit amet lectus dapibus accumsan Fusce gravida tellus quis";
+
+        Map<String, Long> result = Stream.of(text.split(" "))
+                .collect(Collectors.groupingBy(w -> w, Collectors.counting()));
+
+        result.forEach((word, count) -> System.out.println(word + " - " + count));
     }
 }
