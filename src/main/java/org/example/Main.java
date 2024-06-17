6e5c0f3905add9ec1bd65125fb49158d0237b363
Olga8I <olga.filonchik@gmail.com>
2024-06-17 21:20:17 +0300

    Seventh

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            FileInputStream input = new FileInputStream("input.txt");
+            InputStreamReader reader = new InputStreamReader(input);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+
+            FileOutputStream output = new FileOutputStream("output.txt");
+            OutputStreamWriter writer = new OutputStreamWriter(output);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+
+            int currentChar;
+            boolean spaceNeeded = false;
+
+            while ((currentChar = bufferedReader.read()) != -1) {
+                if (Character.isLetter(currentChar)) {
+                    bufferedWriter.write((char) currentChar);
+                    spaceNeeded = true;
+                } else if (currentChar == ' ' && spaceNeeded) {
+                    bufferedWriter.write(' ');
+                    spaceNeeded = false;
+                }
+            }
+
+            bufferedReader.close();
+            bufferedWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
