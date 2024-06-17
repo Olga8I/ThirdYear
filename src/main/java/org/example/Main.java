66b5987caf250665f5857b2ac068b392df47cf8a
Olga8I <olga.filonchik@gmail.com>
2024-06-17 21:04:52 +0300

    First

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter text: ");
+        String text = scanner.nextLine();
+        System.out.println("Enter the letter number to replace: ");
+        int k = scanner.nextInt();
+        System.out.println("Enter the character to replace: ");
+        char symbol = scanner.next().charAt(0);
+
+        String result = Arrays.stream(text.split(" "))
+                .map(word -> {
+                    if (k <= word.length()) {
+                        char[] chars = word.toCharArray();
+                        chars[k - 1] = symbol;
+                        return new String(chars);
+                    } else {
+                        return word;
+                    }
+                })
+                .reduce((acc, word) -> acc + " " + word)
+                .orElse("");
+
+        System.out.println("Result: " + result);
     }
 }
